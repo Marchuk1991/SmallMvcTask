b792ff0ca4f6055c5661d7f268519d1f1cf73bdf
Sergiy Marchuk <sana22_msv@student.ztu.edu.ua>
2023-03-25 15:09:01 +0200

    Add project files.

---
+++ SmallMvcTask/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmallMvcTask.Models;
+using System.Diagnostics;
+
+namespace SmallMvcTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Name(string name)
+        {
+            if (name == "Serhii")
+                return View("Welcome", name);
+
+            return View("Error", name);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
